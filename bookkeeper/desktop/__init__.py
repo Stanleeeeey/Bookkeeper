eb35595f867f68e8d93e421d65283cabc5eb322d
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2025-08-17 14:57:15 +0200

    quick fix

--- bookkeeper/desktop/__init__.py
+++ bookkeeper/desktop/__init__.py
@@ -174,7 +174,6 @@ class DesktopApp: # pylint: disable=too-few-public-methods
         """Starts and runs the app"""
         self._initialize_server()
 
-        self.server.run()
 
-        #webview.create_window('Bookkeeper', self.server, min_size=(600, 600))
-        #webview.start(icon = "static/assets/banner-light.png")
+        webview.create_window('Bookkeeper', self.server, min_size=(600, 600))
+        webview.start(icon = "static/assets/banner-light.png")
